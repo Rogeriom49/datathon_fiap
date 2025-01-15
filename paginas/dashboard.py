01b3f942edfd0e3e70288c73e12eef8b6873a81d
Rogeriom49 <37877711+Rogeriom49@users.noreply.github.com>
2025-01-15 19:56:48 -0300

    Ajuste página dashboard

--- paginas/dashboard.py
+++ paginas/dashboard.py
@@ -4,13 +4,7 @@ from streamlit_js_eval import streamlit_js_eval
 
 st.header('**Dashboard**')
 
-page_width = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
-
-st.write(f'Width: {page_width}')
 
 iframe_url = "https://app.powerbi.com/view?r=eyJrIjoiMGFmNDAzNTEtMWZkYS00NGFiLWEyN2YtMmRkZjIwNjUyZTA2IiwidCI6IjZkYzg3NGNlLWRkMmItNGFhOS05ZjBkLWFkYjkyNjlhNzU4MCJ9 "
 
-col1 = st.columns(1, border=False)
-
-with st.container():
-    components.iframe(iframe_url, width=1400, height=650)
+components.iframe(iframe_url, width=1400, height=650)
