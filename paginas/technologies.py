abbcdad0ae95b8fd2e11b023294191afb2913573
Rogeriom49 <37877711+Rogeriom49@users.noreply.github.com>
2025-01-15 20:08:07 -0300

    inserção do conteúdo da página de tecnologias

---
+++ paginas/technologies.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('**Tecnologias**')
+
+with st.container():
+    st.write('1. **[Streamlit](https://streamlit.io/)** - Criação do MVP (Produto Mínimo Viável) para aplicações interativas e visuais de ciência de dados.  ')
+    st.write('2. **[Pandas](https://pandas.pydata.org/)** - Manipulação e análise de dados com suporte para estruturas de dados como DataFrames.')
+    st.write('3. **[Scikit-learn](https://scikit-learn.org/)** - Treinamento e avaliação de modelos de machine learning, com uma ampla gama de algoritmos prontos para uso.')
+    st.write("4. **[Matplotlib](https://matplotlib.org/)** e **[Plotly](https://plotly.com/)** - Visualização de dados para gráficos estáticos e interativos, respectivamente.")  
+    st.write('5. **[NumPy](https://numpy.org/)** - Funções estatísticas e manipulação de arrays multidimensionais para operações matemáticas eficientes.')
+    st.write('6. **[GitHub](https://github.com/)** - Versionamento do projeto.')
+    st.write('7. **[Power BI](https://powerbi.microsoft.com/)** - Criação do dashboard final.')
