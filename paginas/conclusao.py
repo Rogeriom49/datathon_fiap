89116b82b788ea0e81b03b4a1b9c1270d4e82c1c
Rogeriom49 <37877711+Rogeriom49@users.noreply.github.com>
2025-01-20 19:22:50 -0300

    Inclusão da introduçção e conclusão

---
+++ paginas/conclusao.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title('**Conclusão**')
+
+st.write(""" O objetivo do projeto foi desenvolver uma solução em machine learning utilizando o modelo Support Vector Machine (SVM) para identificar de forma objetiva os alunos com risco de reprovação. A análise foi baseada em um conjunto de dados composto por notas acadêmicas e indicadores de desempenho, viabilizando o planejamento de intervenções educacionais mais assertivas e direcionadas. """)
+st.write(""" O modelo utilizou técnicas robustas para lidar com dados desbalanceados como a técnica de balanceamento SMOTE (Synthetic Minority Over-sampling Technique), aplicada para lidar com classes desbalanceadas em conjuntos de dados. A quantidade de alunos "em risco" era significativamente menor do que a de alunos "não em risco", o que poderia resultar em um modelo com baixa capacidade de previsão para a classe minoritária. Nesse contexto, o SMOTE foi crucial para gerar novos exemplos sintéticos para a classe minoritária, equilibrando o número de exemplos entre as classes e, assim, melhorando o desempenho do modelo. """)
+
+st.divider()
+
+st.subheader("**Benefícios do Modelo**")
+st.write(""" **1. Intervenção Proativa:** Um dos principais benefícios de prever o risco de baixa performance é a capacidade de agir antecipadamente, antes que o aluno alcance um nível crítico de dificuldades acadêmicas. Isso possibilita intervenções mais eficazes e assertivas. """)
+st.write("""**2. Apoio Personalizado:** Com base nas previsões fornecidas pelo modelo, é possível oferecer um suporte personalizado, ajustando as intervenções de acordo com as necessidades individuais de cada aluno, garantindo uma abordagem mais direcionada e eficaz. """)
+st.write(""" **3. Otimização na Alocação de Recursos:** Ao identificar com precisão os alunos que realmente necessitam de apoio, o modelo permite à Passos Mágicos alocar seus recursos de maneira mais estratégica. Isso assegura que a ajuda seja direcionada especificamente aos estudantes em risco, evitando a distribuição indiscriminada de recursos e garantindo um suporte mais eficiente e focado. """)
+st.write(""" **4. Acompanhamento e Monitoramento Contínuo:** O modelo também pode ser utilizado para um acompanhamento constante do desempenho dos alunos ao longo do tempo. À medida que novos dados são coletados, as previsões podem ser atualizadas, ajustando o suporte conforme necessário. Por exemplo, se um aluno inicialmente em risco melhora seu desempenho após a intervenção, o modelo refletirá essa mudança, ajustando a previsão para "não está mais em risco". Esse processo contínuo permite que a instituição acompanhe a eficácia das intervenções e ajuste suas estratégias de forma dinâmica e em tempo real. """)
+st.write(""" Em resumo, o modelo de previsão de risco oferece vantagens significativas, como a intervenção precoce, personalização do suporte, eficiência na alocação de recursos, melhoria da retenção de alunos, e monitoramento contínuo do progresso acadêmico. Essas ações não só ajudam os alunos a alcançarem um melhor desempenho, mas também promovem uma gestão acadêmica mais eficaz e uma instituição mais bem-sucedida no apoio a seus estudantes. """)
+
+st.divider()
+
+st.subheader("**Links Relevantes**")
+st.write(""" - [Repositório no GitHub](https://github.com/Rogeriom49/datathon_fiap)""")
+st.write(""" - [Dashboard](https://app.powerbi.com/view?r=eyJrIjoiMGFmNDAzNTEtMWZkYS00NGFiLWEyN2YtMmRkZjIwNjUyZTA2IiwidCI6IjZkYzg3NGNlLWRkMmItNGFhOS05ZjBkLWFkYjkyNjlhNzU4MCJ9 )""")
--- paginas/introducao.py
+++ paginas/introducao.py
@@ -5,10 +5,47 @@ st.title('**Introdução**')
 tab1, tab2, tab3 = st.tabs(['Sobre a Passos Mágicos', 'Sobre o Projeto', 'Dicionário'])
 
 with tab1:
-    st.write('teste')
+    st.subheader('**Quem somos?**')
+    st.write(""" A Associação Passos Mágicos possui uma história de 31 anos dedicada a transformar a vida de crianças e jovens de baixa renda, oferecendo-lhes melhores perspectivas de futuro. """)
+    st.write(""" Essa iniciativa, idealizada por Michelle Flues e Dimetri Ivanoff, teve início em 1992, com atividades realizadas em orfanatos no município de Embu-Guaçu. """)
+    st.write(""" Em 2016, após anos de experiência e aprendizado, decidiram expandir o alcance do programa para beneficiar um maior número de jovens. Essa transformação, baseada em uma abordagem que combina educação de qualidade, suporte psicológico e psicopedagógico, ampliação da visão de mundo e incentivo ao protagonismo, resultou na criação da Associação Passos Mágicos como um projeto social e educacional. """)
+    st.divider()
+    st.subheader('**O que fazemos?**')
+    st.write(""" A Passos Mágicos transforma vidas por meio da educação, oferecendo aulas de alfabetização, língua portuguesa e matemática para crianças e adolescentes de 7 a 17 anos residentes em Embu-Guaçu. Os estudantes são organizados em turmas de acordo com seu nível de aprendizado, definido por uma avaliação diagnóstica realizada no momento da inscrição. """)
+    st.write(""" Atualmente, a instituição impacta diretamente 1.000 alunos, distribuídos em diferentes etapas de aprendizado: """)
+    st.write(""" - Fase Alfabetização: Para alunos em processo de alfabetização ou com dificuldades na leitura e escrita (20% dos alunos).\n
+- Fases 1 e 2: Aprofundamento dos conteúdos do Ensino Fundamental 1 (37% dos alunos).\n
+- Fases 3 e 4: Foco no Ensino Fundamental 2, com ênfase no aprofundamento de matérias (24% dos alunos).\n
+- Fases 5 e 6: Voltadas para o Ensino Médio, atendendo jovens e adolescentes (8% dos alunos).\n
+- Fases 7 e 8: Preparação intensiva para vestibulares e exames do último ano do Ensino Médio (11% dos alunos). """)
+    st.write("""Além disso, a Passos Mágicos oferece programas educacionais inovadores, como: """)
+    st.write(""" - Preparação para vestibulares e escolas técnicas por meio do programa Vem Ser.\n
+- Cursos avançados em parceria com a USP, incluindo disciplinas como Sustentabilidade, Computação e Programação, oferecendo oportunidades únicas de aprendizado. """)
+    st.write(""" Ciente de que o suporte emocional é essencial para o sucesso acadêmico, a Passos Mágicos também disponibiliza acompanhamento psicológico individual e em grupo para alunos e seus familiares, promovendo bem-estar e equilíbrio emocional para enfrentar os desafios da educação. """)
+    st.divider()
+    st.subheader('**Impacto (indicadores 2023)**')
+    st.write(""" Pessoas Impactadas: **4400** (considerando a média de 4 familiares por aluno) """)
+    st.write(""" Alunos no programa de Aceleração do Conhecimento: **1100 alunos** """)
+    st.write(""" - 20% na alfabetização """)
+    st.write(""" - 37% nas turmas 1 e 2 """)
+    st.write(""" - 24% nas turmas 3 e 4 """)
+    st.write(""" - 8% nas turmas 5 e 6 """)
+    st.write(""" - 11% nas turmas 7 e 8 """)
+    st.write(""" Bolsistas em instituições de ensino particular: **98 alunos** """)
+    st.write(""" - Colégio Evolução Arco Íris: 75 alunos """)
+    st.write(""" - Albert Einstein: 8 alunos """)
+    st.write(""" - Escola João Paulo II: 1 aluno """)
+    st.write(""" - Colégio Poliedro: 2 alunos """)
+    st.write(""" - FIAP: 12 alunos """)
+    st.write(""" Universitários em instituições de ensino superior: **103 alunos** """)
+    st.write(""" - ESPM: 4 alunos """)
+    st.write(""" - Estácio: 5 alunos """)
+    st.write(""" - FIAP: 46 alunos """)
+    st.write(""" - UNISA: 39 alunos """)
 
 with tab2:
-    st.write('teste2')
+    st.write(""" O objetivo do projeto é criar uma solução preditiva para monitorar o desempenho escolar dos alunos, identificando aqueles com resultados acima ou abaixo do esperado. A avaliação será baseada em indicadores como Desempenho Acadêmico, Psicopedagógico, Engajamento, Autoavaliação, Adequação de Nível e Ponto da Virada. Para isso, será empregado o modelo de machine learning que apresentou o melhor desempenho segundo as métricas de F1 Score, Acurácia, Precisão e Recall. """)
+    st.write(""" Adicionalmente, será conduzida uma análise exploratória para evidenciar os impactos gerados pela Passos Mágicos no desempenho dos estudantes. Essa análise levantará indicadores de performance, detalhando a evolução de cada métrica e explorando o comportamento das Pedras, além de ilustrar graficamente os momentos em que o Ponto de Virada ocorre com maior frequência. """)
 
 with tab3:
     col1, col2, col3= st.columns(3)
